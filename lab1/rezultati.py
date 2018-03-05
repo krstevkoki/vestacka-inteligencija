f08234e1ac50c47ce6855b65b69e32741381cc34
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-03-06 00:52:37 +0100

    Add starter code

---
+++ lab1/rezultati.py
@@ -0,0 +1,18 @@
+def suma_kolokviumi(rezultati):
+    pass
+
+
+# your code here
+
+
+if __name__ == "__main__":
+    n = int(input())
+    rezultati = []  # ova e listata od rechnici
+    for i in range(0, n):
+        r = {}  # rechnik koj kje chuva podatoci za eden student
+        brojIndeks = int(input())
+        brojPoeni1 = int(input())
+        brojPoeni2 = int(input())
+        # ovde dodadete gi podatocite vo rechnikot. Potoa dodadete go rechnikot vo listata rezultati!!
+
+    print(suma_kolokviumi(rezultati))
