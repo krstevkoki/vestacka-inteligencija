2850c3e7ab276dc0bc30db7d11e61cc39f370acf
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-03-06 00:48:46 +0100

    Add solution to the problem

--- lab1/tablica2.py
+++ lab1/tablica2.py
@@ -7,4 +7,12 @@ if __name__ == "__main__":
     x = input()
     # vasiot kod pisuvajte go tuka
     tablica = {}
+    scope = range(int(m), int(n) + 1)
+    for i in scope:
+        result = i ** 3
+        tablica[result] = i
+    if int(x) not in tablica.keys():
+        print("nema podatoci")
+    else:
+        print(tablica[int(x)])
     print(sorted(tablica.items()))
