b8c327e6a7732b519100de301b5ab2dbb1406e6d
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-03-06 01:11:34 +0100

    Add solution to the problem

--- lab1/rezultati.py
+++ lab1/rezultati.py
@@ -1,6 +1,10 @@
 def suma_kolokviumi(rezultati):
     # your code here
-    pass
+    for r in rezultati:
+        r["Vkupno od kolokviumi"] = r["Kolokvium 1"] + r["Kolokvium 2"]
+        del r["Kolokvium 1"]
+        del r["Kolokvium 2"]
+    return rezultati
 
 
 if __name__ == "__main__":
@@ -12,5 +16,9 @@ if __name__ == "__main__":
         brojPoeni1 = int(input())
         brojPoeni2 = int(input())
         # ovde dodadete gi podatocite vo rechnikot. Potoa dodadete go rechnikot vo listata rezultati!!
-
+        r["indeks"] = brojIndeks
+        r["Predmet"] = "Veshtachka inteligencija"
+        r["Kolokvium 1"] = brojPoeni1
+        r["Kolokvium 2"] = brojPoeni2
+        rezultati.append(r)
     print(suma_kolokviumi(rezultati))
